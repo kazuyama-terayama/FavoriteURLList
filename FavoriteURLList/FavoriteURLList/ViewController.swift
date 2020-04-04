ec3af7554b466057b280ed062302d26bc1213f4f
寺山和也 <kazuya.outside@outlook.jp>
2020-04-05 01:57:07 +0900

    初回コミット(プログラム記載なし)

---
+++ FavoriteURLList/FavoriteURLList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FavoriteURLList
+//
+//  Created by 寺山和也 on 2020/04/05.
+//  Copyright © 2020 寺山和也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
